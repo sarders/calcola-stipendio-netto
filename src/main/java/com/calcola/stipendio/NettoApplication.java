99b9c5b40c43c9d3a3508763ff9ce1f9692cf722
shahriar.sarder@gmail.com <shahriar.sarder@gmail.com>
2025-03-06 20:55:44 +0100

    init prj

---
+++ src/main/java/com/calcola/stipendio/NettoApplication.java
@@ -0,0 +1,13 @@
+package com.calcola.stipendio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calcola/stipendio/NettoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calcola.stipendio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
